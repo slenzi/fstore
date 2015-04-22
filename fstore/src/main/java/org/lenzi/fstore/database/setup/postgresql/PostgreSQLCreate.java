18c824549bff4f494325e59aff92217db95a214f
slenzi <slenzi@gmail.com>
2015-04-21 21:41:16 -0400

    updated postgresql create scripts

--- fstore/src/main/java/org/lenzi/fstore/database/setup/postgresql/PostgreSQLCreate.java
+++ fstore/src/main/java/org/lenzi/fstore/database/setup/postgresql/PostgreSQLCreate.java
@@ -27,97 +27,97 @@ public class PostgreSQLCreate {
 	protected EntityManager entityManager;	
 	
 	private String SQL_DROP_TABLE_FS_NODE =
-			"drop table FS_NODE";
+			"drop table " + SCHEMA + "FS_NODE";
 		private String SQL_CREATE_TABLE_FS_NODE =
-			"create table FS_NODE ( " +
-			"	NODE_ID NUMBER(15,0) NOT NULL, " +
-			"	PARENT_NODE_ID NUMBER(15,0) NOT NULL, " +
-			"	NAME VARCHAR2(250) NOT NULL, " +
-			"	CREATION_DATE date NOT NULL, " +
-			"	UPDATED_DATE date NOT NULL, " +
+			"create table " + SCHEMA + "FS_NODE ( " +
+			"	NODE_ID NUMERIC(15,0) NOT NULL, " +
+			"	PARENT_NODE_ID NUMERIC(15,0) NOT NULL, " +
+			"	NAME CHARACTER VARYING(250) NOT NULL, " +
+			"	CREATION_DATE TIMESTAMP NOT NULL, " +
+			"	UPDATED_DATE TIMESTAMP NOT NULL, " +
 			"	PRIMARY KEY (NODE_ID) " +
 			")";
 		
 		private String SQL_DROP_TABLE_FS_CLOSURE =
-				"drop table FS_CLOSURE";	
+				"drop table " + SCHEMA + "FS_CLOSURE";	
 		private String SQL_CREATE_TABLE_FS_CLOSURE =
-			"create table FS_CLOSURE ( " +
-			"	LINK_ID NUMBER(15,0) NOT NULL, " +
-			"	PARENT_NODE_ID NUMBER(15,0) NOT NULL, " +
-			"	CHILD_NODE_ID NUMBER(15,0) NOT NULL, " +
-			"	DEPTH NUMBER(5,0) NOT NULL, " +
+			"create table " + SCHEMA + "FS_CLOSURE ( " +
+			"	LINK_ID NUMERIC(15,0) NOT NULL, " +
+			"	PARENT_NODE_ID NUMERIC(15,0) NOT NULL, " +
+			"	CHILD_NODE_ID NUMERIC(15,0) NOT NULL, " +
+			"	DEPTH NUMERIC(5,0) NOT NULL, " +
 			"	PRIMARY KEY (LINK_ID) " +
 			")";
 		
 		// prune table is used in delete operations (e.g., deleteNode(nodeId) method)
 		private String SQL_DROP_TABLE_FS_PRUNE =
-			"drop table FS_PRUNE";	
+			"drop table " + SCHEMA + "FS_PRUNE";	
 		private String SQL_CREATE_TABLE_FS_PRUNE =
-			"create table FS_PRUNE ( " +
-			"	PRUNE_ID NUMBER(15,0) NOT NULL, " +
-			"	NODE_ID NUMBER(15,0) NOT NULL, " +
+			"create table " + SCHEMA + "FS_PRUNE ( " +
+			"	PRUNE_ID NUMERIC(15,0) NOT NULL, " +
+			"	NODE_ID NUMERIC(15,0) NOT NULL, " +
 			"	PRIMARY KEY (PRUNE_ID,NODE_ID) " +
 			")";
 		
 		private String SQL_DROP_TABLE_FS_TREE =
-				"drop table FS_TREE";		
+			"drop table " + SCHEMA + "FS_TREE";		
 		private String SQL_CREATE_TABLE_FS_TREE =
-			"create table FS_TREE ( " +
-			"	TREE_ID NUMBER(15,0) NOT NULL, " +
-			"	ROOT_NODE_ID NUMBER(15,0) NOT NULL, " +
-			"	NAME VARCHAR2(250) NOT NULL, " +
-			"	DESCRIPTION VARCHAR2(2000), " +
-			"	CREATION_DATE date NOT NULL, " +
-			"	UPDATED_DATE date NOT NULL, " +
+			"create table " + SCHEMA + "FS_TREE ( " +
+			"	TREE_ID NUMERIC(15,0) NOT NULL, " +
+			"	ROOT_NODE_ID NUMERIC(15,0) NOT NULL, " +
+			"	NAME CHARACTER VARYING(250) NOT NULL, " +
+			"	DESCRIPTION CHARACTER VARYING(2000), " +
+			"	CREATION_DATE TIMESTAMP NOT NULL, " +
+			"	UPDATED_DATE TIMESTAMP NOT NULL, " +
 			"	PRIMARY KEY (TREE_ID) " +
 			")";	
 		
 		private String SQL_DROP_INDEX_FS_PARENT_DEPTH_CHILD =
-			"drop index fs_parent_depth_child_idx";
+			"drop index " + SCHEMA + "fs_parent_depth_child_idx";
 		private String SQL_CREATE_INDEX_FS_PARENT_DEPTH_CHILD =
-			"create unique index fs_parent_depth_child_idx on fs_closure(parent_node_id,depth,child_node_id)";
+			"create unique index fs_parent_depth_child_idx on " + SCHEMA + "fs_closure(parent_node_id,depth,child_node_id)";
 		
 		private String SQL_DROP_INDEX_FS_CHILD_PARENT_DEPTH =
-			"drop index fs_child_parent_depth_idx";	
+			"drop index " + SCHEMA + "fs_child_parent_depth_idx";	
 		private String SQL_CREATE_INDEX_FS_CHILD_PARENT_DEPTH =
-			"create unique index fs_child_parent_depth_idx on fs_closure(child_node_id,parent_node_id,depth)";	
+			"create unique index fs_child_parent_depth_idx on " + SCHEMA + "fs_closure(child_node_id,parent_node_id,depth)";	
 		
 		private String SQL_DROP_SEQUENCE_FS_NODE_ID =
-			"drop sequence FS_NODE_ID_SEQUENCE";
+			"drop sequence " + SCHEMA + "FS_NODE_ID_SEQUENCE";
 		private String SQL_CREATE_SEQUENCE_FS_NODE_ID =
 			"CREATE SEQUENCE " + SCHEMA + "FS_NODE_ID_SEQUENCE " + 
 			"INCREMENT BY 1 " +
 			"START WITH 1 " +
 			"CACHE 10  " +
-			"NOCYCLE";	
+			"NO CYCLE";	
 		
 		private String SQL_DROP_SEQUENCE_FS_LINK_ID =
-			"drop sequence FS_LINK_ID_SEQUENCE";	
+			"drop sequence " + SCHEMA + "FS_LINK_ID_SEQUENCE";	
 		private String SQL_CREATE_SEQUENCE_FS_LINK_ID =
 			"CREATE SEQUENCE " + SCHEMA + "FS_LINK_ID_SEQUENCE " + 
 			"INCREMENT BY 1 " +
 			"START WITH 1 " +
 			"CACHE 10  " +
-			"NOCYCLE";	
+			"NO CYCLE";	
 		
 		// used in our prune table
 		private String SQL_DROP_SEQUENCE_FS_PRUNE_ID =
-			"drop sequence FS_PRUNE_ID_SEQUENCE";	
+			"drop sequence " + SCHEMA + "FS_PRUNE_ID_SEQUENCE";	
 		private String SQL_CREATE_SEQUENCE_FS_PRUNE_ID =
 			"CREATE SEQUENCE " + SCHEMA + "FS_PRUNE_ID_SEQUENCE " + 
 			"INCREMENT BY 1 " +
 			"START WITH 1 " +
 			"CACHE 10  " +
-			"NOCYCLE";	
+			"NO CYCLE";	
 		
 		private String SQL_DROP_SEQUENCE_FS_TREE_ID =
-			"drop sequence FS_TREE_ID_SEQUENCE";	
+			"drop sequence " + SCHEMA + "FS_TREE_ID_SEQUENCE";	
 		private String SQL_CREATE_SEQUENCE_FS_TREE_ID =
 			"CREATE SEQUENCE " + SCHEMA + "FS_TREE_ID_SEQUENCE " + 
 			"INCREMENT BY 1 " +
 			"START WITH 1 " +
 			"CACHE 10  " +
-			"NOCYCLE";		
+			"NO CYCLE";		
 	
 	public PostgreSQLCreate() {
 		
