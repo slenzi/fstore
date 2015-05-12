156c57ced6af9a6d5aaee1d2b4570594009ad212
slenzi <slenzi@gmail.com>
2015-05-12 14:05:39 -0700

    blah...

---
+++ fstore/src/main/java/org/lenzi/fstore/example/filemanager/service/FileManager.java
@@ -0,0 +1,17 @@
+package org.lenzi.fstore.example.filemanager.service;
+
+import java.io.Serializable;
+
+public interface FileManager extends Serializable {
+
+	// add directory
+	
+	// remove directory
+	
+	// add file
+	
+	// remove file
+	
+	// how to maintain the root directory of the manager?
+	
+}
